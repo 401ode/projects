362b41a9ff9967aa6e28caf8ab96cebccd6e472f
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-10-05 19:52:43 +0000

    Fix removal error. Hopefully.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,8 @@ extensions = [
 
 # Add CommonMark for source-parsing.
 # source_parsers = {
-    '.md': CommonMarkParser,
-}
+#     '.md': CommonMarkParser,
+# }
 
 
 # Add any paths that contain templates here, relative to this directory.
