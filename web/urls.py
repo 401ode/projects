b11afeb8678638212d33a2b77f598ebaf2ef2d84
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-10-25 18:28:29 +0000

    Successful initial implementation. Need to exclude columns.

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from django.http import HttpRequest
 from web.views.home import HomeView
 from web.views.project import ProjectView, projects
 
@@ -7,5 +7,5 @@ from web.views.project import ProjectView, projects
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^p/(?P<pk>[\w/\-]+)?$', ProjectView.as_view(), name='project'),
-    url(r'^/list/$', projects(), name='projects',)
+    url(r'^list/$', projects, name='projects',)
 ]
